3064e11baa31991daac19eb8f2b4e7c193c5486c
Berkay Alkan <berky.alknn@gmail.com>
2025-07-25 01:26:35 +0300

    Add SQL homework 10

---
+++ WEEK3/SQL-HW10.sql
@@ -0,0 +1,16 @@
+----------------Question 1------------------
+
+
+SELECT city.city, country.country FROM city
+LEFT JOIN country ON city.country_id = country.country_id
+
+--------------Question 2-----------------
+
+
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer
+RIGHT JOIN payment ON payment.customer_id = customer.customer_id;
+
+-------------Question 3-------------------
+
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer
+FULL JOIN rental ON rental.customer_id = customer.customer_id;
