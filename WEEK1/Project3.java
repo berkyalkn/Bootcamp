0373b1ac2b4039137cb2e96a2406f2f80908dc0b
Berkay Alkan <berky.alknn@gmail.com>
2025-07-26 11:23:44 +0300

    Add Project3

---
+++ WEEK1/Project3.java
@@ -0,0 +1,27 @@
+
+/* 
+Array : [7, 5, 1, 8, 3, 6, 0, 9, 4, 2]
+
+Binary Search Tree Insertion Steps
+
+7 is added: Since the tree is empty, 7 becomes the root.
+
+5 is added: Since 5 is less than the root (7), it is added to the left of 7.
+
+1 is added: Since 1 is less than 7 (go left) and also less than 5 (go left), it is added to the left of 5.
+
+8 is added: Since 8 is greater than the root (7), it is added to the right of 7.
+
+3 is added: Since 3 is less than 7 (go left), less than 5 (go left), but greater than 1 (go right), it is added to the right of 1.
+
+6 is added: Since 6 is less than 7 (go left) but greater than 5 (go right), it is added to the right of 5.
+
+0 is added: Since 0 is less than 7 (go left), less than 5 (go left), and also less than 1 (go left), it is added to the left of 1.
+
+9 is added: Since 9 is greater than 7 (go right) and also greater than 8 (go right), it is added to the right of 8.
+
+4 is added: Since 4 is less than 7 (go left), less than 5 (go left), greater than 1 (go right), and also greater than 3 (go right), it is added to the right of 3.
+
+2 is added: Since 2 is less than 7 (go left), less than 5 (go left), greater than 1 (go right), but less than 3 (go left), it is added to the left of 3.
+
+*/
